e9bfef01d8b82f0b6a641bf2bf744e7c7f5bf9df
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2021-07-31 20:23:23 -0300

    refactor: update method name

--- src/domain/contracts/channel/create-channel.ts
+++ src/domain/contracts/channel/create-channel.ts
@@ -1,5 +1,5 @@
 import Channel from "../../entities/channel/channel";
 
 export default interface CreateChannel {
-    constructor(id: number, name: string): Channel;
+    create(name: string): Channel;
 }
