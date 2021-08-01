f81ea1480f6f0a90619d3991f3761f1314d3694b
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2021-07-31 21:15:01 -0300

    refactor: rename method name

--- src/domain/contracts/protocol/link-protocol-to-operator.ts
+++ src/domain/contracts/protocol/link-protocol-to-operator.ts
@@ -1,6 +1,3 @@
-import Protocol from "../../entities/protocol/protocol";
-import Operator from "../../entities/operator/operator";
-
 export default interface LinkProtocolToOperator {
-    constructor(protocol: Protocol, operator: Operator): boolean;
+    linkById(protocolId: number, operatorId: number): boolean;
 }
