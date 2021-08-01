aea3729aaf9ca733c40379631f214d2d631391d2
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2021-07-31 21:14:19 -0300

    refactor: rename method name

--- src/domain/contracts/protocol/create-protocol.ts
+++ src/domain/contracts/protocol/create-protocol.ts
@@ -1,6 +1,5 @@
 import Protocol from "../../entities/protocol/protocol";
-import Contact from "../../entities/contact/contact";
 
 export default interface CreateProtocol {
-    constructor(contact: Contact): Protocol;
+    create(data: [string: any][]): Protocol;
 }
