1eabcd52c17a04ef08627d8538f4531ba8ffa68a
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2021-07-31 21:13:21 -0300

    refactor: rename method name

--- src/domain/contracts/protocol/change-protocol-data.ts
+++ src/domain/contracts/protocol/change-protocol-data.ts
@@ -1,5 +1,3 @@
-import Protocol from "../../entities/protocol/protocol";
-
 export default interface ChangeProtocolData {
-    constructor(protocol: Protocol, data: Array<[string: any]>): boolean;
+    updateById(protocolId: number, data: [string: any][]): boolean;
 }
