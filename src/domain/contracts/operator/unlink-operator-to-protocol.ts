f37d4d4bbd67108f99e20862813b4944f8148094
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2021-07-31 21:03:08 -0300

    refactor: rename method name

--- src/domain/contracts/operator/unlink-operator-to-protocol.ts
+++ src/domain/contracts/operator/unlink-operator-to-protocol.ts
@@ -1,3 +1,3 @@
 export default interface UnlinkOperatorToProtocol {
-    constructor(operatorId: number, protocolId: number): boolean;
+    unlinkById(operatorId: number, protocolId: number): boolean;
 }
